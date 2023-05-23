bc802cc86be70ac5d4e9840bcfd3b1792095bd6c
Travis Yeik <tyeik@huskers.unl.edu>
2023-05-23 10:53:02 -0600

    initial

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiCommunityToolkitError;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiCommunityToolkitError;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
